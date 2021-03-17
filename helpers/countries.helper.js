62d1491eb753da025568d9822e6430910b6e8501
David Moreau <dmoreau@lamobilery.fr>
2021-03-17 18:29:56 +0100

    Added tests for filterCountriesWhichHasAnimals

--- helpers/countries.helper.js
+++ helpers/countries.helper.js
@@ -1,16 +1,54 @@
 const { data: countries } = require('../data');
 
-const CountriesHelper = {
-  getCountriesWhereAnimalsNameContains: (nameContains) => {
-    if (typeof nameContains !== 'string') {
-      throw new Error(
-        `nameContains param must be a string. Given ${nameContains}`
-      );
+function getCountriesWithAnimalsNameContaining(animalsNameFilter) {
+  if (typeof animalsNameFilter !== 'string') {
+    throw new Error(
+      `animalsNameFilter param must be a string. Given ${animalsNameFilter}`
+    );
+  }
+
+  const countriesWithFilteredAnimals = countries.map((country) => ({
+    name: country.name,
+    people: getPeopleWithFilteredAnimals(country.people, animalsNameFilter),
+  }));
+
+  return filterCountriesWhichHasAnimals(countriesWithFilteredAnimals);
+}
+
+function getPeopleWithFilteredAnimals(people, animalsNameFilter) {
+  return people.map((person) => ({
+    name: person.name,
+    animals: filterAnimalsWhereNameContaining(
+      person.animals,
+      animalsNameFilter
+    ),
+  }));
+}
+
+function filterAnimalsWhereNameContaining(animals, animalsNameFilter) {
+  return animals.filter((animal) => {
+    if (animal.name.includes(animalsNameFilter)) {
+      return true;
     }
-    return countries;
-  },
-};
+    return false;
+  });
+}
+
+function filterCountriesWhichHasAnimals(countries) {
+  return countries.filter((country) => {
+    const peopleWithAnimals = country.people.filter((person) => {
+      if (person.animals.length === 0) {
+        return false;
+      }
+      return true;
+    });
+    return peopleWithAnimals.length > 0;
+  });
+}
 
 module.exports = {
-  CountriesHelper,
+  getCountriesWithAnimalsNameContaining,
+  getPeopleWithFilteredAnimals,
+  filterAnimalsWhereNameContaining,
+  filterCountriesWhichHasAnimals,
 };
--- helpers/countries.helper.test.js
+++ helpers/countries.helper.test.js
@@ -1,40 +1,106 @@
 const { describe, it, expect } = require('@jest/globals');
-const { CountriesHelper } = require('./countries.helper');
+const {
+  getCountriesWithAnimalsNameContaining,
+  filterCountriesWhichHasAnimals,
+} = require('./countries.helper');
 
-describe('getCountriesWhereAnimalsNameContains', () => {
+describe('getCountriesWithAnimalsNameContaining', () => {
   const animalsNameFilter = 'ry';
 
-  it('should throw an error when nameContains param is not provided', () => {
-    expect(CountriesHelper.getCountriesWhereAnimalsNameContains).toThrow();
+  it('should throw an error when nameContaining param is not provided', () => {
+    expect(getCountriesWithAnimalsNameContaining).toThrow();
   });
 
   it('should give an array', () => {
-    const filteredCountries = CountriesHelper.getCountriesWhereAnimalsNameContains(
+    const filteredCountries = getCountriesWithAnimalsNameContaining(
       animalsNameFilter
     );
     expect(filteredCountries).toBeInstanceOf(Array);
   });
 
-  it('should give an array with at least 1 element', () => {
-    const filteredCountries = CountriesHelper.getCountriesWhereAnimalsNameContains(
+  it('should give at least 1 element', () => {
+    const filteredCountries = getCountriesWithAnimalsNameContaining(
       animalsNameFilter
     );
     expect(filteredCountries.length).toBeGreaterThan(0);
   });
 
-  it('should give an array of countries with their name', () => {
-    const filteredCountries = CountriesHelper.getCountriesWhereAnimalsNameContains(
+  it('should give countries with their name', () => {
+    const filteredCountries = getCountriesWithAnimalsNameContaining(
       animalsNameFilter
     );
     const firstCountry = filteredCountries[0];
     expect(firstCountry).toHaveProperty('name');
   });
 
-  it('should give an array of countries with people', () => {
-    const filteredCountries = CountriesHelper.getCountriesWhereAnimalsNameContains(
+  it('should give countries containing people', () => {
+    const filteredCountries = getCountriesWithAnimalsNameContaining(
       animalsNameFilter
     );
     const firstCountry = filteredCountries[0];
     expect(firstCountry).toHaveProperty('people');
+  });
+
+  it('should give countries containing people containing animals', () => {
+    const filteredCountries = getCountriesWithAnimalsNameContaining(
+      animalsNameFilter
+    );
+    const firstCountry = filteredCountries[0];
+    const firstPerson = firstCountry.people[0];
+    expect(firstPerson).toHaveProperty('animals');
+  });
+
+  it("should give countries where animal's name containing given filter", () => {
+    const filteredCountries = getCountriesWithAnimalsNameContaining(
+      animalsNameFilter
+    );
+    const firstCountry = filteredCountries[0];
+    const firstPerson = firstCountry.people[0];
+    const firstAnimal = firstPerson.animals[0];
+    expect(firstAnimal.name).toContainEqual(animalsNameFilter);
+  });
+});
+
+describe('filterCountriesWhichHasAnimals', () => {
+  const countriesMock = [
+    {
+      name: 'Dillauti',
+      people: [
+        {
+          name: 'Winifred Graham',
+          animals: [],
+        },
+      ],
+    },
+    {
+      name: 'Tohabdal',
+      people: [
+        {
+          name: 'Effie Houghton',
+          animals: [
+            { name: 'Zebra' },
+            { name: 'Ring-tailed Lemur' },
+            { name: 'Fly' },
+            { name: 'Blue Iguana' },
+            { name: 'Emu' },
+            { name: 'African Wild Ass' },
+            { name: 'Numbat' },
+          ],
+        },
+      ],
+    },
+  ];
+
+  it('should give one country', () => {
+    const filteredCountries = filterCountriesWhichHasAnimals(countriesMock);
+    expect(filteredCountries).toHaveLength(1);
+  });
+
+  it('should give a country with animals', () => {
+    const filteredCountries = filterCountriesWhichHasAnimals(countriesMock);
+    const firstCountry = filteredCountries[0];
+    const firstPerson = firstCountry.people[0];
+    console.log({ firstPerson });
+    expect(firstPerson.animals.length).toBeGreaterThan(0);
   });
 });
